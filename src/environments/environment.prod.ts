f6c6dc8cb7d381aa44cebcf4cc14f498c5de48a1
Andrii Vytak <avytak@binariks.com>
2023-05-31 20:57:54 +0300

    Updated env file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
     production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyCxwlHo_b9UmvxuUi5Fx1U-d5wWZ05nDUo",
+        authDomain: "binary-converter-6691f.firebaseapp.com",
+        projectId: "binary-converter-6691f",
+        storageBucket: "binary-converter-6691f.appspot.com",
+        messagingSenderId: "336357681493",
+        appId: "1:336357681493:web:5dc7faa5905613735c67dc"
+    }
 
 }
